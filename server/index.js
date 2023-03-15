bd42a5ca41f3948cdb94198de72888988ebd07b6
yingzhuo <yingzhuo1994@gmail.com>
2023-03-14 23:05:09 -0500

    import dotenv and router

--- server/index.js
+++ server/index.js
@@ -3,15 +3,23 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+import postRoutes from "./routes/posts.js";
+import dotenv from 'dotenv';
+dotenv.config();
+
 const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.get('/', (req, res) => {
+    res.send('hello world')
+  })
+app.use('/posts', postRoutes);
+
 
 const CONNECTION_URL = process.env.DB_URL;
-
 const PORT = process.env.PORT || 5000;
 
 mongoose
@@ -20,5 +28,6 @@ mongoose
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
   })
   .catch((error) => {
+    console.log(CONNECTION_URL)
     console.log(error.message);
   });
