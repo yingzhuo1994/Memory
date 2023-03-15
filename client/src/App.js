eb19b39fb23167057f51ff71de12c3a8e315400a
yingzhuo <yingzhuo1994@gmail.com>
2023-03-14 23:52:56 -0500

    add image, Form, and Posts

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,38 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+import Posts from './components/posts/Posts';
+import Form from './components/Form/Form';
+import memories from './images/memories.png';
 
 const App = () => {
-    return (
-        <div>
-            <h1>App</h1>
-        </div>
-    )
-}
+  return (
+    <Container maxidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Memories
+        </Typography>
+        <img src={memories} alt="memories" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justifyContent="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
+};
 
 export default App;
