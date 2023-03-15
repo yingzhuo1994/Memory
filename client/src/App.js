5a433f7295714aaa2cac48ba7771450bc673145b
yingzhuo <yingzhuo1994@gmail.com>
2023-03-15 13:34:22 -0500

    add posts

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,33 @@
-import React from "react";
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import React, { useEffect } from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import { useDispatch } from "react-redux";
 
-import Posts from './components/posts/Posts';
+import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
+import { getPosts } from './actions/posts';
+import useStyles from './styles.js';
 import memories from './images/memories.png';
-import useStyles from './styles';
 
 const App = () => {
-    const classes = useStyles();
+  const dispatch = useDispatch();
+  const classes = useStyles();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
+
   return (
     <Container maxidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
         <Typography className={classes.heading} variant="h2" align="center">
           Memories
         </Typography>
-        <img className={classes.image} src={memories} alt="memories" height="60" />
+        <img
+          className={classes.image}
+          src={memories}
+          alt="memories"
+          height="60"
+        />
       </AppBar>
       <Grow in>
         <Container>
