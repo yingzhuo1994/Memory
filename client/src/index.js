30a40513cd7f9ca25255675379b682b1fc623eab
yingzhuo <yingzhuo1994@gmail.com>
2023-03-16 23:09:10 -0500

    fix createRoot issue

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as ReactDOMClient from 'react-dom/client';
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
@@ -10,9 +11,18 @@ import './index.css';
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+const container = document.getElementById('root');
+
+// Create a root
+const root = ReactDOMClient.createRoot(container);
+
+root.render(  <Provider store={store}>
+  <App />
+</Provider>);
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+//   document.getElementById("root")
+// );
