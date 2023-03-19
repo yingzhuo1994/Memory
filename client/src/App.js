23230b84e5cf01e4ea5641e3799d8233eda9e9d9
yingzhuo <yingzhuo1994@gmail.com>
2023-03-19 03:15:00 -0500

    backend setup

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ import PostDetails from "./components/PostDetails/PostDetails";
 
 const App = () => {
   const user = JSON.parse(localStorage.getItem('profile'));
-  console.log("app test: ", user);
+  // console.log("app test: ", user);
 
   return (
     <GoogleOAuthProvider clientId="">
--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000'});
+// const API = axios.create({ baseURL: 'http://localhost:5000'});
+const API = axios.create({ baseURL: 'https://memory-n0u7.onrender.com/'});
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
